749a65c44c1a92f2115f6769af98bb05d3b8150f
John Boesen <82904895+JohnBoesen12309@users.noreply.github.com>
2022-09-18 20:27:06 -0400

    Add files via upload

---
+++ ECE 122/Project 4/pi.py
@@ -0,0 +1,16 @@
+##################################################
+###### IF YOU WORK BY GROUP OF TWO ###############
+###### ENTER YOUR TWO NAMES HERE:  ###############
+###### Name1=
+###### Name2=
+######
+###### Only 1 submission on moodle (either names ok)
+##################################################
+
+import numpy
+import matplotlib.pyplot as plt
+
+for i in range(10):
+    x=
+
+
